30618aeece11bae1f2bda2c07408488176450c84
Skorodinskaya <skorodinskaya01@gmail.com>
2021-10-03 13:36:43 +0300

    changes

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
-const fs = require('fs');
+const fs = require ('fs');
 const path = require('path');
 
 const mkdirBoys = path.join(__dirname, 'users', 'boys');
 const mkdirGirls = path.join(__dirname, 'users', 'girls');
 
-fs.mkdir(mkdirBoys, {recursive: true}, (err) => {
+fs.mkdir(mkdirBoys, {recursive: true}, err => {
     console.log(err);
-})
-fs.mkdir(mkdirGirls, {recursive: true}, (err) => {
+});
+
+fs.mkdir(mkdirGirls, {recursive: true}, err => {
     console.log(err);
-})
+});
+
+const sorter = (currentFolder, gender, futureFolder) => {
+    fs.readdir(currentFolder, (err, data) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+
+        data.forEach(value => {
+            fs.readFile(path.join(currentFolder, value), (err, data)=> {
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+
+                const item = JSON.parse(data);
+                if(item.gender === gender){
+                    fs.rename(path.join(currentFolder, value), path.join(futureFolder, value), (err) => {
+                        if(err){
+                            console.log(err);
+                        }
+                    })
+                }
+            })
+        })
+    })
+}
+
+sorter(mkdirBoys, 'female', mkdirGirls);
+sorter(mkdirGirls, 'male', mkdirBoys);
